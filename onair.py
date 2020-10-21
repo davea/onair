e68a1a540ae30419951300cfb9f84084344d837c
Dave Arter <davea@mysociety.org>
2020-10-21 11:02:54 +0100

    More reliably kill helper when exiting

--- onair.py
+++ onair.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
-import os
+from dotenv import load_dotenv
+load_dotenv()
 
-from sh import mosquitto_pub, Command
+import os
+import atexit
+
+from sh import mosquitto_pub, pkill, Command
 
 MQTT_BROKER = os.environ['MQTT_BROKER']
 MQTT_TOPICS = os.environ['MQTT_TOPICS']
@@ -13,18 +17,27 @@ def send_message(on_air):
         mosquitto_pub("-h", MQTT_BROKER, "-t", topic, "-m", payload)
 
 def main():
+    kill_helper() # tidy up any stale helpers
     onair_helper = Command("./onair_helper")(_iter=True)
     try:
         for line in onair_helper:
             if line.find("Camera active") > -1:
-                print("Switching on because:", line)
+                print("Switching on because:", line, flush=True)
                 send_message(True)
             elif line.find("Camera inactive") > -1:
-                print("Switching off because:", line)
+                print("Switching off because:", line, flush=True)
                 send_message(False)
     finally:
+        print("Exiting", flush=True)
         onair_helper.terminate()
 
+@atexit.register
+def kill_helper():
+    print("kill_helper", flush=True)
+    try:
+        pkill("onair_helper")
+    except Exception:
+        pass
 
 if __name__ == "__main__":
     main()
